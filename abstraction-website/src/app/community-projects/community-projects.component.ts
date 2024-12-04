e1cca2d55702331d634d9f0bd49780da7e82855c
Chaerry22 <dchae22@gmail.com>
2024-12-04 16:54:30 -0500

    save

---
+++ abstraction-website/src/app/community-projects/community-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-community-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './community-projects.component.html',
+  styleUrl: './community-projects.component.css'
+})
+export class CommunityProjectsComponent {
+
+}
