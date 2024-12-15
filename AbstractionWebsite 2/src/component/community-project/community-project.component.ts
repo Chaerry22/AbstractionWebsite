231caa26d984f5559532a26adffc445a57089fd1
Joshua Park <sp72@calvin.edu>
2024-12-14 19:52:43 -0500

    commit

--- AbstractionWebsite 2/src/component/community-project/community-project.component.ts
+++ AbstractionWebsite 2/src/component/community-project/community-project.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProjectsComponent } from '../projects/projects.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
     selector: 'app-community-project',
     standalone: true,
-    imports: [ProjectsComponent],
+    imports: [ProjectsComponent, RouterLink],
     templateUrl: './community-project.component.html',
     styleUrl: './community-project.component.css'
 })
--- AbstractionWebsite 2/src/component/workshops/workshops.component.ts
+++ AbstractionWebsite 2/src/component/workshops/workshops.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectsComponent } from '../projects/projects.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-workshops',
     standalone: true,
-
+    imports:[ProjectsComponent, RouterLink],
     templateUrl: './workshops.component.html',
     styleUrl: './workshops.component.css',
 
